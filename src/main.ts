796fba35b81b4fc572fbe67595cb66c47f591408
C0m91ex <jtorr124@ucsc.edu>
2024-10-13 20:14:16 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Incremental Game Idea";
 document.title = gameName;
 
+// Create header
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Create a button with an emoji
+const button = document.createElement("button");
+button.innerHTML = "🥞";
+app.append(button);
